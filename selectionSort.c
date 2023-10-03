db0a6d27eff3d70936d32d127417a879ab412a26
Lucas Sachetto <lucashsachetto@hotmail.com>
2023-10-03 18:22:58 -0300

    adicionando algoritimos quick sort e selection sort

---
+++ selectionSort.c
@@ -0,0 +1,18 @@
+void selectionSort(int arr[], int n)
+{
+    int i, menorIndice, j, trocaValor;
+
+    for(i=0; i < n-1; i++) {
+        menorIndice = i;
+
+        for (j=i+1; j < n; j++) {
+            if (arr[j] < arr[menorIndice]) {
+                menorIndice = j; 
+            }
+        }
+
+        if (menorIndice != i) {
+            troca(&arr[i], &arr[menorIndice]);
+        }
+    }
+}
---
+++ util.c
@@ -0,0 +1,8 @@
+// Troca o valor de dois inteiros
+void troca(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
