e305f605f9e682147d33fb6013cd8690a42d4777
root <root@Antonio>
2023-10-05 23:29:49 -0300

    feat: add random partition function in quick sort

--- quickSort.c
+++ quickSort.c
@@ -17,6 +17,21 @@ int particione(int arr[], int indiceInicio, int indiceFim)
     troca(&arr[i + 1], &arr[indiceFim]);
     return (i + 1);
 }
+
+int getRandomNumber(int lower, int upper) {
+    int num = (rand() % (upper - lower + 1)) + lower;
+    return num;
+}
+
+int particioneAleatorio(int arr[], int indiceInicio, int indiceFim) {
+    srand(time(NULL));
+
+    int i = getRandomNumber(indiceInicio, indiceFim);
+
+    troca(&arr[i + 1], &arr[indiceFim]);
+
+    return particione(arr, indiceInicio, indiceFim);
+}
  
 void quickSort(int arr[], int indiceInicio, int indiceFim)
 {
@@ -29,7 +44,23 @@ void quickSort(int arr[], int indiceInicio, int indiceFim)
     }
 }
 
+void quickSortAleatorio(int arr[], int indiceInicio, int indiceFim)
+{
+    if (indiceInicio < indiceFim) {
+
+        int q = particioneAleatorio(arr, indiceInicio, indiceFim);
+
+        quickSort(arr, indiceInicio, q - 1);
+        quickSort(arr, q + 1, indiceFim);
+    }
+}
+
 void quickStarter(int arr[], int n)
 {
     quickSort(arr, 0, n-1);
+}
+
+void quickStarterAleatorio(int arr[], int n)
+{
+    quickSortAleatorio(arr, 0, n-1);
 }
